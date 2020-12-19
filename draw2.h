4363e27c1e960624f0e9d48eff69341aedd78180
August25thDD <72698094+August25thDD@users.noreply.github.com>
2020-12-19 22:51:39 +0800

    mfcDrawApplication
    简易绘画板，可以实现直线、矩形、椭圆的绘制，更改线条颜色，图形填充色，有保存的功能。初学mfc写的项目，功能有待添加，适合初学者练习学习。

---
+++ draw2.h
@@ -0,0 +1,32 @@
+﻿
+// draw2.h: draw2 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// Cdraw2App:
+// 有关此类的实现，请参阅 draw2.cpp
+//
+
+class Cdraw2App : public CWinApp
+{
+public:
+	Cdraw2App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cdraw2App theApp;
---
+++ resource.h
@@ -0,0 +1,54 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 draw2.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_draw2TYPE                   130
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_LINE                         32782
+#define ID_RECT                         32783
+#define ID_ELLPSE                       32784
+#define ID_W1                           32785
+#define ID_W2                           32786
+#define ID_W3                           32787
+#define ID_W4                           32788
+#define ID_W5                           32789
+#define ID_W6                           32790
+#define ID_COLOR                        32791
+#define ID_FULLCOLOR                    32792
+#define ID_32793                        32793
+#define ID_32794                        32794
+#define ID_32795                        32795
+#define ID_32796                        32796
+#define ID_32797                        32797
+#define ID_32798                        32798
+#define ID_32799                        32799
+#define ID_P1                           32800
+#define ID_P2                           32801
+#define ID_P3                           32802
+#define ID_P4                           32803
+#define ID_P5                           32804
+#define ID_DRAW_SAVE                    32811
+#define ID_POLY                         32812
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32813
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
